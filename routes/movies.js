2648ed772b2a1cf5e4b9680ab6689c4f151583b9
WJChoong <choong@massiveinfinity.com>
2021-09-09 18:59:59 +0800

    change route into separate folder

---
+++ routes/movies.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+
+let movies = [
+    {
+        id: 1,
+        name: "The Notebook",
+        artist: "Ryan Gosling"
+      },
+      {
+        id: 2,
+        name: "The Hate U Give",
+        artist: "Amandla Stenberg"
+      },
+      {
+        id: 3,
+        name: "Titanic",
+        artist: "Jackson Maine"
+      },
+      {
+        id: 4,
+        name: "A Star Is Born",
+        artist: "Bradley Cooper"
+      }
+];
+
+router.param("movieId", (req, res,next, id) => {
+    let movie = movies.find(movie => movie.id == parseInt(req.params.movieId));
+    req.movie = movie;
+    next();
+})
+
+router.get('/', (req, res) => {
+    res.status(200).json(movies);
+})
+
+//create a new movie, and return new movie
+router.post('/', (req, res) => {
+  let newmovie = {
+    id: movies.length + 1,
+    name: req.body.name,
+    artist: req.body.artist 
+  }
+  movies.push(newmovie);
+  res.status(201).json(newmovie);
+});
+
+//return a movie with id 
+router.get('/:movieId', (req, res) => {
+    res.status(200).json(req.movie);
+});
+
+//edit a movie with id, and return edited movie
+router.put('/:movieId', (req, res) => {
+    req.movie.name = req.body.name;
+    req.movie.artist = req.body.artist;
+    res.status(200).json(req.movie);
+});
+
+//delete a movie with id, and return deleted movie
+router.delete("/:movieId", (req, res) => {
+    let index = movies.indexOf(req.movie);
+    movies.splice(index, 1);
+    res.status(200).json(req.movie);
+});
+
+module.exports = router;
---
+++ routes/songs.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+
+let songs = [
+    {
+      id: 1,
+      name: "Before You Go",
+      artist: "Lewis Capaldi"
+    },
+    {
+      id: 2,
+      name: "Tap In",
+      artist: "Saweetie"
+    },
+    {
+      id: 3,
+      name: "Say Something",
+      artist: "Kylie Minogue"
+    },
+    {
+      id: 4,
+      name: "More Than My Hometown",
+      artist: "Morgan Wallen"
+    }
+];
+
+router.param('songId', (req, res, next, id) => {
+    let song = songs.find(song => song.id === parseInt(id));
+    req.song = song;
+    next();
+});
+
+// Get all songs
+router.get('/', (req, res, next) => {
+    res.status(200).json(songs);
+});
+
+// Add new songs 
+router.post('/', (req, res) => {
+    let newSong = {
+        id: songs.length + 1,
+        name: req.body.name,
+        artist: req.body.artist 
+    }
+    songs.push(newSong);
+    res.status(201).json(newSong);
+});
+
+//  Get songs with id
+router.get('/:songId/', (req, res, next) =>{
+    res.status(200).json(req.song);
+})
+
+//edit a song with id, and return edited song
+router.put('/:songId', (req, res) => {
+  req.song.name = req.body.name;
+  req.song.artist = req.body.artist;
+  res.status(200).json(req.song);
+});
+
+//delete a song with id, and return deleted song
+router.delete("/:songId", (req, res) => {
+    console.log("1");
+    let songToDelete = req.song
+    let index = songs.indexOf(songToDelete);
+    songs.splice(index, 1);
+    res.status(200).json(songToDelete);
+});
+
+module.exports = router;
