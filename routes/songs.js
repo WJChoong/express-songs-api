7196ed9b184b73c7e2cedc79b2732ce3915b2086
WJChoong <choong@massiveinfinity.com>
2021-09-10 12:16:29 +0800

    add error handler

--- routes/songs.js
+++ routes/songs.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-let songs = [];
+let songs = [
+    {
+      id: 1,
+      name: "Before You Go",
+      artist: "Lewis Capaldi"
+    },
+    {
+      id: 2,
+      name: "Tap In",
+      artist: "Saweetie"
+    },
+    {
+      id: 3,
+      name: "Say Something",
+      artist: "Kylie Minogue"
+    },
+    {
+      id: 4,
+      name: "More Than My Hometown",
+      artist: "Morgan Wallen"
+    }
+];
 
-//Song API
-router.param('id', (req, res, next, id) => {
+router.param('songId', (req, res, next, id) => {
     let song = songs.find(song => song.id === parseInt(id));
-    req.song = song;
-    next();
+    if (songs.id){
+        req.song = song;
+        next();
+    }else{
+        res.send({ error: `Unable to find song with id: ${id}`})
+    }
 });
 
-//return list of all songs
-router.get('/', (req, res) => {
+// Get all songs
+router.get('/', (req, res, next) => {
     res.status(200).json(songs);
 });
-  
-//create a new song, and return new song
+
+// Add new songs 
 router.post('/', (req, res) => {
     let newSong = {
         id: songs.length + 1,
@@ -25,21 +49,22 @@ router.post('/', (req, res) => {
     songs.push(newSong);
     res.status(201).json(newSong);
 });
-  
-//return a song with id 
-router.get('/:id', (req, res) => {
+
+//  Get songs with id
+router.get('/:songId/', (req, res, next) =>{
     res.status(200).json(req.song);
-});
-  
+})
+
 //edit a song with id, and return edited song
-router.put('/:id', (req, res) => {
-    req.song.name = req.body.name;
-    req.song.artist = req.body.artist;
-    res.status(200).json(req.song);
+router.put('/:songId', (req, res) => {
+  req.song.name = req.body.name;
+  req.song.artist = req.body.artist;
+  res.status(200).json(req.song);
 });
-  
+
 //delete a song with id, and return deleted song
-router.delete("/:id", (req, res) => {
+router.delete("/:songId", (req, res) => {
+    console.log("1");
     let songToDelete = req.song
     let index = songs.indexOf(songToDelete);
     songs.splice(index, 1);
